019479d18c85d181f774dcfb2876dc538aab5a4f
Joscop24 <jorisbourdin.pro@gmail.com>
2023-02-03 14:00:06 +0100

    Composant ColleagueList Fini

--- src/app/shared/components/colleague/colleague.component.ts
+++ src/app/shared/components/colleague/colleague.component.ts
@@ -1,23 +1,42 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Colleague} from "../../../models/colleague";
-/*import {pseudo, score, photo} from '../../../models/colleague'*/
+import {LikeHate} from "../../../models/like-hate";
+
 
 @Component({
   selector: 'tc-colleague',
   templateUrl: './colleague.component.html',
   styleUrls: ['./colleague.component.scss']
 })
-export class ColleagueComponent {
 
-/*
-  @Input() pseudo = "Thierry";
-  @Input() score = "100";
-  @Input() photo = "https://randomuser.me/api/portraits/men/24.jpg";
-*/
 
-  @Input() collegue: Colleague = {
-    pseudo: "Jean marc",
-    score: 100,
+
+export class ColleagueComponent implements OnInit {
+  @Input() collegue:Colleague ={
+    pseudo: "Jean",
+    score: -999,
     photo:"https://randomuser.me/api/portraits/men/22.jpg"
+  }
+
+    likeButtonDisabled = false;
+    hateButtonDisabled = false;
+
+  updateScore(likeHate: LikeHate){
+    switch (likeHate){
+      case LikeHate.LIKE:
+        this.collegue.score ++;
+        break;
+      case LikeHate.HATE:
+        this.collegue.score --;
+    }
+    this.likeHateDisableling();
+  }
+
+  likeHateDisableling() {
+    this.likeButtonDisabled = this.collegue.score >= 1000;
+    this.hateButtonDisabled = this.collegue.score <= -1000;
+  }
+
+  ngOnInit(): void {
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LikeHateComponent } from './components/like-hate/like-hate.component';
 import { ColleagueComponent } from './components/colleague/colleague.component';
+import { ColleagueListComponent } from './components/colleague-list/colleague-list.component';
 
 @NgModule({
   declarations: [
     LikeHateComponent,
-    ColleagueComponent
+    ColleagueComponent,
+    ColleagueListComponent
   ],
   exports: [
     LikeHateComponent,
-    ColleagueComponent
+    ColleagueComponent,
+    ColleagueListComponent
   ],
   imports: [
     CommonModule
