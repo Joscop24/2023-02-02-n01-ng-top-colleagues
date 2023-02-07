237ee26c884a9b078ff2797f2ab17626a81b68bd
Joscop24 <jorisbourdin.pro@gmail.com>
2023-02-07 11:15:21 +0100

    VoteService fini

--- src/app/shared/components/voting-history/voting-history.component.ts
+++ src/app/shared/components/voting-history/voting-history.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Colleague} from "../../../models/colleague";
 import {Vote} from "../../../models/vote";
 import {LikeHate} from "../../../models/like-hate";
+import {VoteService} from "../../../providers/vote.service";
 
 @Component({
   selector: 'tc-voting-history',
@@ -9,58 +10,10 @@ import {LikeHate} from "../../../models/like-hate";
   styleUrls: ['./voting-history.component.scss']
 })
 export class VotingHistoryComponent {
-  laure:Colleague = {
-    pseudo: "lolo",
-    score: 200,
-    photo: "https://randomuser.me/api/portraits/women/64.jpg"
-  }
-  lucas:Colleague = {
-    pseudo: "lukreator",
-    score: -940,
-    photo: "https://randomuser.me/api/portraits/men/32.jpg"
-  }
-  eli:Colleague = {
-    pseudo: "strifey",
-    score: 900,
-    photo: "https://randomuser.me/api/portraits/men/2.jpg"
-  }
-  joris:Colleague = {
-    pseudo: "Jojo",
-    score: 2,
-    photo: "https://randomuser.me/api/portraits/men/18.jpg"
-  }
-  antoine:Colleague = {
-    pseudo: "TavTav",
-    score: 651,
-    photo: "https://randomuser.me/api/portraits/men/29.jpg"
+
+  constructor(private voteService: VoteService) {
   }
 
-
-  vote1:Vote ={
-    collegue: this.joris,
-    vote : LikeHate.HATE
-  }
-  vote2:Vote ={
-    collegue: this.laure,
-    vote : LikeHate.LIKE
-  }
-  vote3:Vote ={
-    collegue: this.lucas,
-    vote : LikeHate.LIKE
-  }
-  vote4:Vote ={
-    collegue: this.joris,
-    vote : LikeHate.HATE
-  }
-
-  tabVotes: Vote[] = [this.vote1,this.vote2,this.vote3,this.vote4]
-
-  suppr(id:number){
-    this.tabVotes.splice(id,1);
-
-
-  }
-
-
+  votes: Vote[] = this.voteService.list();
 
 }
